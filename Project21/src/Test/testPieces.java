b43f5a9f37a084e2670f368c51f4d31ad0ef11e3
Jan Super <Jan Super>
2020-09-15 16:07:43 +0200

    small initial test

--- Project21/src/Test/testPieces.java
+++ Project21/src/Test/testPieces.java
@@ -7,6 +7,9 @@ public class testPieces {
 	public static void main(String[] args) {
 		Piece testpiece = new BlackPiece();
 		Piece testpiece2 = new WhitePiece();
+		
+		System.out.println(testpiece.getVisual());
+		System.out.println(testpiece2.getVisual());
 	}
 
 }
