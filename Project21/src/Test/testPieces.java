5dad4ab59636ca370817ce74ab391ac80d7cb4dd
Jan Super <Jan Super>
2020-09-15 16:03:29 +0200

    test package + piece test

---
+++ Project21/src/Test/testPieces.java
@@ -0,0 +1,12 @@
+package Test;
+
+import Piece.*;
+
+public class testPieces {
+	
+	public static void main(String[] args) {
+		Piece testpiece = new BlackPiece();
+		Piece testpiece2 = new WhitePiece();
+	}
+
+}
