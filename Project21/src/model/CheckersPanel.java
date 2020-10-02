4decd5ad8cbff311fd516f84655a76573c625d88
hahoangviet <hahoangviet126@gmail.com>
2020-10-02 11:45:38 +0200

    game logic

---
+++ Project21/src/model/CheckersPanel.java
@@ -0,0 +1,77 @@
+package model;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class CheckersPanel extends JPanel {
+
+    // Invariant: board != null
+
+    Board board;
+    Color squareBlack = new Color (0, 0, 0);
+    Color squareWhite = new Color (255, 255, 255);
+    Color pieceBlack = new Color (0, 0, 0);
+    Color pieceWhite = new Color (255, 255, 255);
+
+    // Pre: None
+    // Post: Constructs the canvas
+    public CheckersPanel () {
+	super();
+	board = new Board();
+    }
+
+    public Board getBoard() {return board;}
+
+    // Pre: None
+    // Post: Draws a visual representation of a Board corresponding
+    //       to what board represents
+    protected void paintComponent (Graphics g) {
+
+	int squareWidth = getWidth() / board.numCols();
+	int squareHeight = getHeight() / board.numRows();
+
+	for (int row = board.minRow(); row <= board.maxRow(); ++row) {
+	    for (int col = board.minCol(); col <= board.maxCol(); ++col) {
+		if (board.isWhiteSquare (row, col)) {
+		    g.setColor (squareWhite);
+		} else {
+		    g.setColor (squareBlack);
+		}
+		g.fillRect (col * squareWidth, row * squareHeight, 
+			    squareWidth, squareHeight);
+
+		if (!board.noPieceAt (row, col)) {
+		    if (board.pieceAt (row, col, Board.WHITE)) {
+			g.setColor (pieceWhite);
+
+		    } else if (board.pieceAt (row, col, Board.BLACK)) {
+			g.setColor (pieceBlack);
+		    }
+		    g.fillOval (col * squareWidth, row * squareHeight, 
+				squareWidth, squareHeight);
+		    g.setColor (Color.white);
+		    g.drawOval (col * squareWidth, row * squareHeight, 
+				squareWidth - 1, squareHeight - 1);
+		    if (board.kingAt (row, col)) {
+			g.drawOval (col * squareWidth + squareWidth / 4,
+				    row * squareHeight + squareHeight / 4,
+				    squareWidth / 2, squareHeight / 2);
+		    }
+		}
+	    }
+	}
+    }
+
+    // Pre: 0 <= yClick < getHeight()
+    // Post: Returns corresponding board row
+    public int getRow (int yClick) {
+	return yClick * board.numRows() / getHeight(); 
+    }
+
+    // Pre: 0 <= xClick < getWidth()
+    // Post: Returns corresponding board column
+    public int getCol (int xClick) {
+	return xClick * board.numCols() / getWidth();
+    }
+    
+}
---
+++ Project21/src/model/Game.java
@@ -0,0 +1,101 @@
+package model;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+public class Game extends JFrame {
+	
+	private JButton startGame;
+    private CheckersPanel board;
+    private JLabel message;
+
+	public Game() {
+        setTitle("Checkers");
+        setSize(500, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        Container pane = getContentPane();
+        pane.setLayout(new BorderLayout());
+        
+        board = new CheckersPanel();
+        board.addMouseListener(new CheckerListener());
+        pane.add(board, BorderLayout.CENTER);
+        
+        JPanel top = new JPanel(new GridLayout(4,1));
+        pane.add(top, BorderLayout.NORTH);
+        
+        JPanel buttons = new JPanel(new FlowLayout());
+        top.add(buttons);
+        
+        startGame = new JButton("Start new game");
+        startGame.addActionListener(new GameStarter());
+        buttons.add(startGame);
+//        
+//        computerMove = new JButton("Computer move");
+//        computerMove.addActionListener(new ComputerMover());
+//        buttons.add(computerMove);
+//        
+        message = new JLabel();
+        buttons.add(message);
+	}
+	
+	private class GameStarter implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            restart();
+        }
+    }
+	
+	private void restart() {
+        board.getBoard().reset();
+        board.repaint();
+       	changeTurn();
+    }
+	
+	private void changeTurn () {
+        if (board.getBoard().isWinner(Board.BLACK)) {
+            message.setText ("Black wins!");
+        } else if (board.getBoard().isWinner(Board.WHITE)) {
+            message.setText ("White wins!");
+        } else if (board.getBoard().isTurnOf(Board.WHITE)) {
+            message.setText ("White's turn");
+        } else {
+            message.setText ("Black's turn");
+        }
+    }
+	
+	private class CheckerListener extends MouseAdapter {
+        private int colStart, rowStart;
+        private boolean awaitingRelease;
+        
+        public void mousePressed (MouseEvent e) {
+            colStart = board.getCol(e.getX());
+            rowStart = board.getRow(e.getY());
+            awaitingRelease = !board.getBoard().noPieceAt(rowStart, colStart);
+            if (!awaitingRelease) {
+                JOptionPane.showMessageDialog(null, "Illegal move");
+            }
+        }
+        
+//        public void mouseReleased (MouseEvent e) {
+//            if (awaitingRelease) {
+//                Set<Move> legal = board.getBoard().getCurrentPlayerMoves();
+//                Move mouseMove = new Move(board.getBoard(), rowStart, colStart,
+//                board.getRow (e.getY()), 
+//                board.getCol (e.getX()));
+//                
+//                if (legal.contains(mouseMove)) {
+//                    makeMove(mouseMove);
+//                } else {
+//                    JOptionPane.showMessageDialog(null, "Illegal move");
+//                }
+//            } 
+//        }
+    }
+	
+	public static void main(String[] args) {
+        Game c = new Game();
+        c.setVisible(true);
+    }
+}
